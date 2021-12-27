7a54c81d2f47dfaf8a6e9e47d7cb7f85acd1c0ce
Staubgeborener <mail@staubgeborener.de>
2021-12-27 15:20:15 +0100

    Update HowToUse.sh

--- lazydocker/HowToUse.sh
+++ lazydocker/HowToUse.sh
@@ -1,5 +1,5 @@
 # add lazydocker command as alias
-echo "alias lazydocker='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ./config:/.config/jesseduffield/lazydocker --name lazydocker lazyteam/lazydocker'" >> ~/.bashrc
+echo "alias lazydocker='sudo docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ~/config:/.config/jesseduffield/lazydocker --name lazydocker lazyteam/lazydocker'" >> ~/.bashrc
 source ~/.bashrc
 
 # run lazydocker
