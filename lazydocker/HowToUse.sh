bfe07f7a4303615d70e0b1b3fc8b52e95d11ca0f
Staubgeborener <eric.schroeder@protonmail.com>
2024-01-20 10:01:36 +0100

    cleanup - unused containers deleted

--- lazydocker/HowToUse.sh
+++
@@ -1,6 +0,0 @@
-# add lazydocker command as alias
-echo "alias lazydocker='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ~/config:/.config/jesseduffield/lazydocker --name lazydocker lazyteam/lazydocker'" >> ~/.bashrc
-source ~/.bashrc
-
-# run lazydocker
-lazydocker
